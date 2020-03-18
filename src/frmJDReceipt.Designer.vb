4b10f26cdf2c2004cc278360b6f99d683a13008e
towerbit <42426532+towerbit@users.noreply.github.com>
2020-03-18 10:52:09 +0800

    Add files via upload

---
+++ src/frmJDReceipt.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmJDReceipt
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmJDReceipt))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.txtUrl = New System.Windows.Forms.ToolStripTextBox()
+        Me.btnGo = New System.Windows.Forms.ToolStripButton()
+        Me.btnParse = New System.Windows.Forms.ToolStripButton()
+        Me.btnDetailPage = New System.Windows.Forms.ToolStripButton()
+        Me.btnMainPage = New System.Windows.Forms.ToolStripButton()
+        Me.wb = New System.Windows.Forms.WebBrowser()
+        Me.wb2 = New System.Windows.Forms.WebBrowser()
+        Me.btnChangeTitle = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.txtUrl, Me.btnGo, Me.btnParse, Me.btnDetailPage, Me.btnMainPage, Me.btnChangeTitle})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1605, 27)
+        Me.ToolStrip1.TabIndex = 0
+        '
+        'txtUrl
+        '
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(900, 27)
+        Me.txtUrl.Text = "https://myivc.jd.com/fpzz/index.action"
+        '
+        'btnGo
+        '
+        Me.btnGo.Image = CType(resources.GetObject("btnGo.Image"), System.Drawing.Image)
+        Me.btnGo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(54, 24)
+        Me.btnGo.Text = "Go"
+        '
+        'btnParse
+        '
+        Me.btnParse.Image = CType(resources.GetObject("btnParse.Image"), System.Drawing.Image)
+        Me.btnParse.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnParse.Name = "btnParse"
+        Me.btnParse.Size = New System.Drawing.Size(147, 24)
+        Me.btnParse.Text = "Auto Download"
+        '
+        'btnDetailPage
+        '
+        Me.btnDetailPage.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.btnDetailPage.CheckOnClick = True
+        Me.btnDetailPage.Image = CType(resources.GetObject("btnDetailPage.Image"), System.Drawing.Image)
+        Me.btnDetailPage.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnDetailPage.Name = "btnDetailPage"
+        Me.btnDetailPage.Size = New System.Drawing.Size(115, 24)
+        Me.btnDetailPage.Text = "Detail Page"
+        '
+        'btnMainPage
+        '
+        Me.btnMainPage.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.btnMainPage.Checked = True
+        Me.btnMainPage.CheckOnClick = True
+        Me.btnMainPage.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.btnMainPage.Image = CType(resources.GetObject("btnMainPage.Image"), System.Drawing.Image)
+        Me.btnMainPage.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnMainPage.Name = "btnMainPage"
+        Me.btnMainPage.Size = New System.Drawing.Size(109, 24)
+        Me.btnMainPage.Text = "Main Page"
+        '
+        'wb
+        '
+        Me.wb.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wb.Location = New System.Drawing.Point(0, 27)
+        Me.wb.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wb.Name = "wb"
+        Me.wb.ScriptErrorsSuppressed = True
+        Me.wb.Size = New System.Drawing.Size(1605, 637)
+        Me.wb.TabIndex = 1
+        '
+        'wb2
+        '
+        Me.wb2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wb2.Location = New System.Drawing.Point(0, 0)
+        Me.wb2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wb2.Name = "wb2"
+        Me.wb2.Size = New System.Drawing.Size(1605, 664)
+        Me.wb2.TabIndex = 2
+        '
+        'btnChangeTitle
+        '
+        Me.btnChangeTitle.Image = CType(resources.GetObject("btnChangeTitle.Image"), System.Drawing.Image)
+        Me.btnChangeTitle.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnChangeTitle.Name = "btnChangeTitle"
+        Me.btnChangeTitle.Size = New System.Drawing.Size(124, 24)
+        Me.btnChangeTitle.Text = "Change Title"
+        '
+        'frmJDReceipt
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1605, 664)
+        Me.Controls.Add(Me.wb)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.wb2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmJDReceipt"
+        Me.Text = "JDReceipter"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents txtUrl As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents btnGo As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btnParse As System.Windows.Forms.ToolStripButton
+    Friend WithEvents wb As WebBrowser
+    Friend WithEvents wb2 As WebBrowser
+    Friend WithEvents btnDetailPage As ToolStripButton
+    Friend WithEvents btnMainPage As ToolStripButton
+    Friend WithEvents btnChangeTitle As ToolStripButton
+End Class
---
+++ src/frmJDReceipt.vb
@@ -0,0 +1,267 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+
+Public Class frmJDReceipt
+
+    Protected Overrides Sub OnLoad(e As EventArgs)
+        MyBase.OnLoad(e)
+        wb.AllowNavigation = True
+        wb.AllowWebBrowserDrop = True
+        AddHandler wb.Navigated, AddressOf wbNavigatedEventHandler
+
+        Dim axWb As SHDocVw.WebBrowser = CType(wb.ActiveXInstance, SHDocVw.WebBrowser)
+        AddHandler axWb.NewWindow2, AddressOf axWbNewWindow2EventHandler
+        AddHandler wb2.DocumentCompleted, AddressOf wbDocumentCompletedEventHandler
+    End Sub
+
+    Private Sub axWbNewWindow2EventHandler(ByRef ppDisp As Object, ByRef Cancel As Boolean)
+        '打开新窗口的操作定向到指定的 WebBrowser
+        ppDisp = wb2.ActiveXInstance
+    End Sub
+
+    Private Sub wbNavigatedEventHandler(sender As Object, e As WebBrowserNavigatedEventArgs)
+        txtUrl.Text = e.Url.ToString
+    End Sub
+
+    Private _currElemet As HtmlElement
+
+    Private Sub wbDocumentCompletedEventHandler(sender As Object, e As WebBrowserDocumentCompletedEventArgs)
+        Dim browser As WebBrowser = CType(sender, WebBrowser)
+
+        'Const WDFP_URL As String = "https://myivc.jd.com/fpzz/index.action"
+        Const FPXQ_URL As String = "https://myivc.jd.com/fpzz/ivcLand.action?orderId="
+        'Const CKFP_URL As String = "https://storage.jd.com/eicore-fm.jd.com/"
+        Const HKFP_URL As String = "https://myivc.jd.com/fpzz/hkfpReq.action"
+
+        Dim url As String = e.Url.ToString
+        Dim doc As HtmlDocument = browser.Document
+        If url.StartsWith(FPXQ_URL) Then
+            '下载发票
+            _currElemet = getCKFPElement(doc)
+            If _currElemet IsNot Nothing Then
+                url = _currElemet.GetAttribute("href")
+                Call saveFile(url, doc)
+            Else
+                Debug.Print("Skip for FPXQ url is empty")
+            End If
+
+            If _fpxq IsNot Nothing Then
+                If _fpxq.Count > 0 Then
+                    '继续
+                    'If MessageBox.Show("continue to next?", Me.Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
+                    url = _fpxq.Dequeue
+                    _currElemet = findElement(wb.Document, url, "发票详情")
+                    If _currElemet IsNot Nothing Then
+                        _currElemet.InvokeMember("click")
+                    Else
+                        Debug.Print("findElement NOT FOUND " & url)
+                    End If
+                    'End If
+                Else
+                    wb.BringToFront()
+                    MessageBox.Show("all done")
+                    _fpxq = Nothing
+                    'ToolStrip1.Enabled = True
+                End If
+            End If
+        ElseIf url.StartsWith(HKFP_URL) Then
+            '换开发票：填写表单并自动提交
+            'ivcTitleType.value="5"
+            browser.BringToFront()
+            btnDetailPage.Checked = True
+            btnMainPage.Checked = False
+
+            _currElemet = findElement(doc, "ivcTitleType")
+            _currElemet.SetAttribute("value", "5")
+            'company.value=""
+            _currElemet = findElement(doc, "company")
+            _currElemet.SetAttribute("value", My.Settings.ReceiptTitle)
+            'taxNo.value=""
+            _currElemet = findElement(doc, "taxNo")
+            _currElemet.SetAttribute("value", My.Settings.TaxCode)
+
+            _currElemet = findElement(doc, "javascript:void(0)", "提交")
+            If _currElemet IsNot Nothing Then
+                If MessageBox.Show("Sure to submit the request of changing receipt?"，
+                                    Me.Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
+                    _currElemet.InvokeMember("click")
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        wb.Navigate(txtUrl.Text)
+    End Sub
+
+    ''' <summary>
+    ''' 发票详情 Url 队列
+    ''' </summary>
+    Private _fpxq As Queue(Of String)
+
+    Private Sub btnParse_Click(sender As Object, e As EventArgs) Handles btnParse.Click
+        _fpxq = getFPXQUrls(wb.Document)
+        If _fpxq.Count > 0 Then
+            'If MessageBox.Show("start?", Me.Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
+            'ToolStrip1.Enabled = False
+            _currElemet = findElement(wb.Document, _fpxq.Dequeue, "发票详情")
+            _currElemet.InvokeMember("click")
+
+            wb2.BringToFront()
+            'End If
+        Else
+            MessageBox.Show("FPXQ not found")
+        End If
+    End Sub
+
+    Private Sub btnMainPage_Click(sender As Object, e As EventArgs) Handles btnMainPage.Click
+        wb.BringToFront()
+        btnDetailPage.Checked = False
+    End Sub
+
+    Private Sub btnDetailPage_Click(sender As Object, e As EventArgs) Handles btnDetailPage.Click
+        wb2.BringToFront()
+        btnMainPage.Checked = False
+    End Sub
+
+    ''' <summary>
+    ''' 下载保存文件
+    ''' </summary>
+    ''' <param name="url"></param>
+    Private Shared Sub saveFile(ByVal url As String, doc As HtmlDocument)
+        Dim strCookies As String = doc.Cookie
+        'If String.IsNullOrEmpty(strCookies) Then
+        '    strCookies = CookieHelper.GetCookieString(url)
+        'End If
+        Try
+            Using wc As New Net.WebClient
+                wc.Headers.Add("Cookie", strCookies) '共享 WebBrowser.Cookie
+                wc.DownloadFile(url, getFilename(url))
+            End Using
+        Catch ex As Exception
+            Stop
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' 通过 Url 截取文件名
+    ''' </summary>
+    ''' <param name="url"></param>
+    ''' <param name="ext">扩展名， 默认为 .pdf</param>
+    ''' <returns></returns>
+    ''' <example> 
+    ''' url = https://storage.jd.com/eicore-fm.jd.com/012001900311-54851690.pdf?Expires=2529543048&AccessKey=bfac05320eaf11cc80cf1823e4fb87d98523fc94&Signature=ezQY7fu9DNVPArELUFpXbOqjOu8%3D
+    ''' </example>
+    Private Shared Function getFilename(ByVal url As String, ByVal Optional ext As String = ".pdf") As String
+        Dim ret As String = String.Empty
+        Dim p As Int32 = url.LastIndexOf("/")
+        If p > "https://".Length Then
+            Dim t As Int32 = url.IndexOf(ext, p)
+            If t > 0 Then
+                ret = url.Substring(p + 1, t - p - 1) & ext
+            End If
+        End If
+        Return ret
+    End Function
+
+    'Private Class CookieHelper
+    '    <DllImport("wininet.dll", CharSet:=CharSet.Auto, SetLastError:=True)>
+    '    Shared Function InternetGetCookieEx(ByVal pchURL As String, ByVal pchCookieName As String, ByVal pchCookieData As StringBuilder, ByRef pcchCookieData As Integer, ByVal dwFlags As Integer, ByVal lpReserved As Object) As Boolean
+    '    End Function
+
+    '    Public Shared Function GetCookieString(ByVal url As String) As String
+    '        ' Determine the size of the cookie
+    '        Dim datasize As Integer = 256
+    '        Dim cookieData As New StringBuilder(datasize)
+
+    '        If Not InternetGetCookieEx(url, Nothing, cookieData, datasize, &H2000, Nothing) Then
+    '            If datasize < 0 Then
+    '                Return Nothing
+    '            End If
+
+    '            ' Allocate stringbuilder large enough to hold the cookie
+    '            cookieData = New StringBuilder(datasize)
+    '            If Not InternetGetCookieEx(url, Nothing, cookieData, datasize, &H2000, Nothing) Then
+    '                Return Nothing
+    '            End If
+    '        End If
+    '        Return cookieData.ToString()
+    '    End Function
+    'End Class
+
+    Private Shared Function findElement(ByVal doc As HtmlDocument, ByVal elementId As String) As HtmlElement
+        Dim ret As HtmlElement = Nothing
+        Try
+            ret = doc.GetElementById(elementId)
+        Catch ex As Exception
+            Debug.Print("findElement err:{0}", ex.Message)
+        End Try
+
+        Return ret
+    End Function
+
+    Private Shared Function findElement(doc As HtmlDocument, ByVal href As String, ByVal innerText As String) As HtmlElement
+        Dim ret As HtmlElement = Nothing
+        For Each element As HtmlElement In doc.Links
+            If element.InnerText = innerText AndAlso element.GetAttribute("href") = href Then
+                ret = element
+                Exit For
+            End If
+        Next
+        Return ret
+    End Function
+
+    Private Shared Function getFPXQUrls(ByVal doc As HtmlDocument) As Queue(Of String)
+        Dim ret As New Queue(Of String)
+
+        For Each element As HtmlElement In doc.Links
+            If element.InnerText = "发票详情" Then
+                ret.Enqueue(element.GetAttribute("href"))
+            End If
+        Next
+
+        Return ret
+    End Function
+
+    Private Shared Function getCKFPElement(ByVal doc As HtmlDocument) As HtmlElement
+        Dim ret As HtmlElement = Nothing
+
+        For Each element As HtmlElement In doc.Links
+            If element.InnerText = "查看发票" Then
+                ret = element '.Parent
+                Exit For
+            End If
+        Next
+
+        Return ret
+    End Function
+
+    Private Sub btnChangeTitle_Click(sender As Object, e As EventArgs) Handles btnChangeTitle.Click
+        'Todo: 自动填写换开表单并提交
+        btnChangeTitle.Checked = Not btnChangeTitle.Checked
+        If btnChangeTitle.Checked Then
+            '填写换开抬头后税号
+            Dim sInput As String = Nothing
+            sInput = InputBox("The receipt title change to:", Me.Text, My.Settings.ReceiptTitle)
+            If Not String.IsNullOrEmpty(sInput) Then
+                My.Settings.ReceiptTitle = sInput
+
+                sInput = InputBox("The tax code change to:", Me.Text, My.Settings.TaxCode)
+                If Not String.IsNullOrEmpty(sInput) Then
+                    My.Settings.TaxCode = sInput
+                    My.Settings.Save()
+                    MessageBox.Show("Ready to change receipt title to " & My.Settings.ReceiptTitle，
+                                    Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("The tax code can't be empty"，
+                                    Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                End If
+            Else
+                MessageBox.Show("The receipt title can't be empty"，
+                                Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End If
+        End If
+    End Sub
+End Class
